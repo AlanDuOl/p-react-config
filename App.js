cc8d8f01adb3a05a065c0a1719e778e56a445507
alan <alanduarte90@gmail.com>
2019-05-20 17:30:29 -0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Square from './src/components/Square.jsx'
+
+class App extends Component {
+	render(){
+		return (
+			<Square />
+		);
+	}
+}
+
+export default App;
---
+++ src/components/Square.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Square extends Component {
+	render(){
+		return (
+			<div>
+				<p> I will be a square </p>
+			</div>
+		);
+	}
+}
+
+export default Square;
